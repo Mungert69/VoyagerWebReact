e65e41f1372e20aa1489c057ef3f380216811782
mungert <mungert@gmail.com>
2018-10-09 18:52:10 +0100

    test mungert69

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -6,7 +6,7 @@ import TripContainer from '../containers/TripContainer';
 
 export class AppContainer extends Component {
     displayName = "App Container";
-
+var test;
     render() {
         return (
             <Grid fluid>
