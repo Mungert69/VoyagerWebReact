61c2604e731c45dca57ad097153b984c3eb8cc45
unknown <voyagerworldtravel@gmail.com>
2018-11-05 15:26:43 +0000

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router';
 import TripContainer  from './containers/TripContainer';
 import { AppContainer } from './components/AppContainer';
 import { TestView } from './components/TestView';
-import { Control_All } from './components/development_parts/Control_All.js';
+
 import './voyager.css';
 require('./voyager.css');
 
@@ -17,7 +17,7 @@ export default class App extends Component {
 
             <Route path='/tripcontainer' component={TripContainer} />
             <Route path='/testview' component={TestView} />
-            <Route path='./components/development_parts/Control_All.js' component={Control_All} />
+ 
       </AppContainer>
     );
   }
