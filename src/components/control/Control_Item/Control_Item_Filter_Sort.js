ef92f0c7baac57a0deaeaa5d96c4a7395e1de831
unknown <voyagerworldtravel@gmail.com>
2018-11-15 22:46:32 +0000

    .

--- src/components/control/Control_Item/Control_Item_Filter_Sort.js
+++ src/components/control/Control_Item/Control_Item_Filter_Sort.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 export class Control_Item_Filter_Sort extends Component {
   render() {
     return (
-            <div className="Show">
-              <a className="Control_Item_A" onClick={() => this.props.changeView("filter_sort")}>
-                <i className="fas fa-filter Control_Icon_Size_2" />
-                <span className="Control_Text_Size_1_Link">filter & Sort</span>
-              </a>
-            </div>
+            <div className="show">
+            <a className="Control_Item_A" onClick={() => this.props.changeView("filter_sort")}>
+          <span>
+              <i className="fas fa-filter Control_Icon_Layer_Sidebyside"></i>
+              <i className="fas fa-sort Control_Icon_Layer_Sidebyside"></i>
+          </span>
+          <span className="Control_Text_Size_1_Link" >filter & Sort</span>
+      </a>
+  </div>
     );
   }
 }
