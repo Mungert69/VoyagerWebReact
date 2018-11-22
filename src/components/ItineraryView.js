918a4ad579d6579a6abb5f22665ddfa480e57ece
Mungert69 <mungert@gmail.com>
2018-11-22 17:43:37 +0000

    trying to fix merge conflict

--- src/components/ItineraryView.js
+++ src/components/ItineraryView.js
@@ -3,7 +3,7 @@ import { HotelList } from '../components/HotelList';
 import { CardView } from '../components/CardView';
 import '../voyager.css';
 import { Select, Button } from 'antd';
-import {apiBaseUrl} from './Constants';
+import { apiBaseUrl } from './Constants';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
@@ -31,8 +31,8 @@ export class ItineraryView extends Component {
     }
 
     refreshPrSelections = () => {
-       
-            this.props.fetchStoredItinObj(this.props.userId);
+
+        this.props.fetchStoredItinObj(this.props.userId);
     }
 
     passBackClick() {
@@ -43,7 +43,7 @@ export class ItineraryView extends Component {
 
     deleteHotel = () => {
         // Add Hotel api call
-        let str = apiBaseUrl+'api/Itinerary/DelHotel/'+ this.props.userId + '/';
+        let str = apiBaseUrl + 'api/Itinerary/DelHotel/' + this.props.userId + '/';
         fetch(str)
             .then(response => response.json())
             .then(result => {
@@ -57,7 +57,7 @@ export class ItineraryView extends Component {
 
     addNight = (index) => {
         // Add Hotel api call
-        let str = apiBaseUrl+'api/Itinerary/AddNight/' + index + '/'+ this.props.userId + '/';
+        let str = apiBaseUrl + 'api/Itinerary/AddNight/' + index + '/' + this.props.userId + '/';
         fetch(str)
             .then(response => response.json())
             .then(result => {
@@ -71,7 +71,7 @@ export class ItineraryView extends Component {
 
     removeNight = (index) => {
         // Add Hotel api call
-        let str =apiBaseUrl+ 'api/Itinerary/RemoveNight/' + index + '/'+ this.props.userId + '/';
+        let str = apiBaseUrl + 'api/Itinerary/RemoveNight/' + index + '/' + this.props.userId + '/';
         fetch(str)
             .then(response => response.json())
             .then(result => {
@@ -89,7 +89,7 @@ export class ItineraryView extends Component {
 
 
     renderPRSelections = (itinObj) => {
-        if (itinObj===undefined) return;
+        if (itinObj === undefined) return;
         // Only render if there are items in the itinerary.
         if (itinObj.prSelections !== undefined && itinObj.prSelections !== null && itinObj.prSelections.length) {
             return (
@@ -98,52 +98,53 @@ export class ItineraryView extends Component {
 
                 <PerfectScrollbar className="Control_Trip_Stages_Scrollbar">
 
-<div className="Control_Trip_Stages">
+                    <div className="Control_Trip_Stages">
 
-<p className="Voyager_Trip_Transfer" > {itinObj.transferNodeItems[0].transferItem.transferItem1}</p>{itinObj.prSelections.map((prSelection, index) =>
+                        <p className="Voyager_Trip_Transfer" > {itinObj.transferNodeItems[0].transferItem.transferItem1}</p>
+                        {itinObj.prSelections.map((prSelection, index) =>
 
-<div key={index}>
-                       
-<a className="Control_Trip_Stages_Item_Group AnimationRipple" >
-<div class="Control_Trip_Stages_Position_outer_wrapper">
-<div className="Control_Trip_Stages_Position_left Control_Trip_Stages_Position_inner_wrapper">
-<i className="show fas fa-circle Control_Trip_Stages_Stage_Icon"><l className="Control_Trip_Stages_Stage_Icon_Number">{index}</l></i>
-                       
-<a className="Control_Trip_Stages_Stage_PlaceName" onClick={() => this.props.setBuilderCardEvent(null, index, 'place')}>{prSelection.place}</a>
-</div>
+                            <div key={index}>
 
-<div className="Control_Trip_Stages_Position_right Control_Trip_Stages_Position_inner_wrapper">
-<a className="Control_Trip_Stages_Stage_HotelName" onClick={() => this.props.setBuilderCardEvent(null, index, 'hotel')}>{prSelection.hotel}</a>
-</div>
-</div>
-<div class="Control_Trip_Stages_Position_outer_wrapper">
-<div className="Control_Trip_Dates_Position_left Control_Trip_Stages_Position_inner_wrapper">
-<i className="show far fa-car Control_Trip_Stages_Stage_Transport_Icon"></i>
-<i className="hide far fa-bus Control_Trip_Stages_Stage_Transport_Icon"></i>
-</div>
+                                <a className="Control_Trip_Stages_Item_Group AnimationRipple" >
+                                    <div class="Control_Trip_Stages_Position_outer_wrapper">
+                                        <div className="Control_Trip_Stages_Position_left Control_Trip_Stages_Position_inner_wrapper">
+                                            <i className="show fas fa-circle Control_Trip_Stages_Stage_Icon"><l className="Control_Trip_Stages_Stage_Icon_Number">{index}</l></i>
 
-<div className="Control_Trip_Stages_Position_right Control_Trip_Stages_Position_inner_wrapper">
-<span className="Control_Trip_Stages_Stage_HotelFeatures">CITY CENTRE - SMALL HOTEL - HERITAGE - £££</span>
-<i className="show fas fa-moon Control_Trip_Stages_Stage_Nights_Icon"></i>         
-<a className="Voyager_Trip_Control_Nights_add" title="ADD NIGHTS" onClick={() => this.addNight(index)} ><i className="fas fa-angle-up Voyager_Trip_Nights_Add"></i></a>
-{prSelection.nights}
-<a className="Voyager_Trip_Control_Nights_remove" title="SUBTRACT NIGHTS" onClick={() => this.removeNight(index)} ><i className="fas fa-angle-down Voyager_Trip_Nights_Subtract"></i></a>
-<a className="Voyager_Trip_Control_Delete" title="DELETE STAGE" onClick={() => this.deleteHotel()} ><i className="fas fa-times Voyager_Trip_Hotel_Remove"></i></a> <i className="fas fa-bus Voyager_Trip_Transport_Icon"></i>
-</div>
-</div>
-</a><div>
-</div>
-<a><i className="show fas fa-caret-right Control_Trip_Stages_Stage_Detail_Icon" onClick={() => this.props.setBuilderCardEvent(null, index, 'place')}></i></a>
+                                            <a className="Control_Trip_Stages_Stage_PlaceName" onClick={() => this.props.setBuilderCardEvent(null, index, 'place')}>{prSelection.place}</a>
+                                        </div>
 
-<p className="Voyager_Trip_Transfer" > {itinObj.transferNodeItems[index+1].transferItem.transferItem1}</p>
-</div>
+                                        <div className="Control_Trip_Stages_Position_right Control_Trip_Stages_Position_inner_wrapper">
+                                            <a className="Control_Trip_Stages_Stage_HotelName" onClick={() => this.props.setBuilderCardEvent(null, index, 'hotel')}>{prSelection.hotel}</a>
+                                        </div>
+                                    </div>
+                                    <div class="Control_Trip_Stages_Position_outer_wrapper">
+                                        <div className="Control_Trip_Dates_Position_left Control_Trip_Stages_Position_inner_wrapper">
+                                            <i className="show far fa-car Control_Trip_Stages_Stage_Transport_Icon"></i>
+                                            <i className="hide far fa-bus Control_Trip_Stages_Stage_Transport_Icon"></i>
+                                        </div>
 
-)}
-<p className="Voyager_Trip_Transfer" > {itinObj.transferNodeItems[itinObj.transferNodeItems.length-1].transferItem.transferItem1}</p>
+                                        <div className="Control_Trip_Stages_Position_right Control_Trip_Stages_Position_inner_wrapper">
+                                            <span className="Control_Trip_Stages_Stage_HotelFeatures">CITY CENTRE - SMALL HOTEL - HERITAGE - £££</span>
+                                            <i className="show fas fa-moon Control_Trip_Stages_Stage_Nights_Icon"></i>
+                                            <a className="Voyager_Trip_Control_Nights_add" title="ADD NIGHTS" onClick={() => this.addNight(index)} ><i className="fas fa-angle-up Voyager_Trip_Nights_Add"></i></a>
+                                            {prSelection.nights}
+                                            <a className="Voyager_Trip_Control_Nights_remove" title="SUBTRACT NIGHTS" onClick={() => this.removeNight(index)} ><i className="fas fa-angle-down Voyager_Trip_Nights_Subtract"></i></a>
+                                            <a className="Voyager_Trip_Control_Delete" title="DELETE STAGE" onClick={() => this.deleteHotel()} ><i className="fas fa-times Voyager_Trip_Hotel_Remove"></i></a> <i className="fas fa-bus Voyager_Trip_Transport_Icon"></i>
+                                        </div>
+                                    </div>
+                                </a><div>
+                                </div>
+                                <a><i className="show fas fa-caret-right Control_Trip_Stages_Stage_Detail_Icon" onClick={() => this.props.setBuilderCardEvent(null, index, 'place')}></i></a>
 
-</div>
+                                <p className="Voyager_Trip_Transfer" > {itinObj.transferNodeItems[index + 1].transferItem.transferItem1}</p>
+                            </div>
 
-</PerfectScrollbar>
+                        )}
+                        <p className="Voyager_Trip_Transfer" > {itinObj.transferNodeItems[itinObj.transferNodeItems.length - 1].transferItem.transferItem1}</p>
+
+                    </div>
+
+                </PerfectScrollbar>
 
 
             );//return
