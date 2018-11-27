0494e84e1e95e38d0466cfd8e27ad4d1fc635617
Mungert69 <mungert@gmail.com>
2018-11-27 16:58:58 +0000

    Added priceStr toTrip Date and Price component

--- src/components/control/Control_Item/Control_Item_Trip_Date_Price.js
+++ src/components/control/Control_Item/Control_Item_Trip_Date_Price.js
@@ -3,7 +3,7 @@ export const Control_Item_Trip_Date_Price = (props) => {
   if (props.travelDate === undefined || props.travelDate === null) return <p>Date N/A</p>;
     return (
       <div>
-      <span className="Trip_Date_Price">{props.travelDate} £{props.price}</span>
+      <span className="Trip_Date_Price">{props.travelDate} {props.priceStr}</span>
       </div>
     );
   
