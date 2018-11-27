73fa58de7c429441f27dce6c6c2e5f4f4153ff84
Mungert69 <mungert@gmail.com>
2018-11-27 17:49:42 +0000

    removed travelDate from Control_Item_Trip_Date_Price the priceStr has dates in it now

--- src/components/control/Menus/MenuTripListView.js
+++ src/components/control/Menus/MenuTripListView.js
@@ -47,7 +47,7 @@ const MenuTripListView = (props) => {
                     </div>
 
                     <div className="Control_Position_center Control_Position_inner_wrapper inline-block">
-                        <Control_Item_Trip_Date_Price travelDate={props.card.travelDate} priceStr={props.card.priceStr} />
+                        <Control_Item_Trip_Date_Price  priceStr={props.card.priceStr} />
                         <Control_Item_Country_Name country={props.card.country}/>
                         <Control_Item_Hotel_Place_Country_Name />                     
                     </div>
