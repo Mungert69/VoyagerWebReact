f3c9ec413ce79eeeb37f5e86cef55fc0fe8882cd
Mungert69 <mungert@gmail.com>
2018-11-28 21:31:34 +0000

    added new datePriceObjs props to MenuTripListView

--- src/components/control/Control_Item/Control_Item_Trip_Date_Price.js
+++ src/components/control/Control_Item/Control_Item_Trip_Date_Price.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-export const Control_Item_Trip_Date_Price = (props) => {
-  if (props.pricesObjs === undefined || props.pricesObjs === null) return <p>Date N/A</p>;
+export const Control_Item_Trip_Date_Price = ({datePricesObjs}) => {
+  if (datePricesObjs === undefined || datepricesObjs === null) return <p>Date N/A</p>;
     return (
       <div>
       {
-      props.priceObjs.map(priceObj => <span> <span className="Trip_Date_Price"> {priceObj.date}</span>
-      <span className="Trip_Date_Price"> {priceObj.price}</span></span>)
+      datePriceObjs.map(datePriceObj => <span> <span className="Trip_Date_Price"> {datePriceObj.date}</span>
+      <span className="Trip_Date_Price"> {datePriceObj.price}</span></span>)
       }
     
       </div>
