ebf04932af3f84106380dcfae5f9168fc9783ac5
Mungert69 <mungert@gmail.com>
2018-11-28 21:45:18 +0000

    Fixed layout of Control_Item_date_price

--- src/components/control/Control_Item/Control_Item_Trip_Date_Price.js
+++ src/components/control/Control_Item/Control_Item_Trip_Date_Price.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-export const Control_Item_Trip_Date_Price = ({datePriceObjs}) => {
-  if (datePriceObjs === undefined || datePriceObjs === null) return <p>Date N/A</p>;
-    return (
-      <div>
+export const Control_Item_Trip_Date_Price = ({ datePriceObjs }) => {
+  if (datePriceObjs === undefined || datePriceObjs === null)
+    return <p>Date N/A</p>;
+  return (
+    <div>
       <span className="Trip_Date_Price">
-      {
-      datePriceObjs.map(datePriceObj => <span>{datePriceObj.dateStr} {datePriceObj.price}</span> 
-      )
-      }
+        {datePriceObjs.map(datePriceObj => (
+          <span>
+            {datePriceObj.dateStr} {datePriceObj.price}
+          </span>
+        ))}
       </span>
-      </div>
-    );
-  
-}
+    </div>
+  );
+};
